076c2552f2a67f7019270631a63f482440ced53a
Madhav Mundhra <madhavmundhra221@gmail.com>
2025-01-09 23:15:43 +0530

    Integrated Project users and collaborations on frontend

--- frontend/src/screens/Project.jsx
+++ frontend/src/screens/Project.jsx
@@ -1,14 +1,147 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom';
+import axios from '../config/axios.js';
 
 const Project = () => {
 
     const location = useLocation();
 
-    console.log(location.state);
+    const [isSidePanelOpen, setIsSidePanelOpen] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [selectedUserId, setSelectedUserId] = useState([]);
+    const [project, setProject] = useState(location.state.project);
+
+    const [users, setUsers] = useState([]);
+
+    const handleUserClick = (id) => {
+        setSelectedUserId(prevSelectedUserId => {
+            const newSelectedUserId = new Set(prevSelectedUserId);
+            if (newSelectedUserId.has(id)) {
+                newSelectedUserId.delete(id);
+            } else {
+                newSelectedUserId.add(id);
+            }
+            return newSelectedUserId;
+        });
+    }
+
+    function addCollaborators() {
+        axios.put('/projects/add-user',{
+            projectId: location.state.project._id,
+            users: Array.from(selectedUserId)
+        })
+        .then(res => {
+            // console.log(res.data);
+            setIsModalOpen(false);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    }
+
+    useEffect(() => {
+
+        axios.get(`/projects/get-project/${location.state.project._id}`)
+        .then(res => {
+            console.log(res.data.project);
+            setProject(res.data.project);
+        })
+
+        axios.get('/users/all')
+        .then(res => {
+
+            setUsers(res.data.users);
+
+        })
+        .catch(err => {
+            console.log(err);
+        })
+     }, [])
+
+    // console.log(location.state);
 
     return (
-        <div>Project</div>
+        <main className='h-screen w-screen flex'>
+            <section className='left relative flex flex-col h-full min-w-96 bg-slate-300'>
+
+                <header className='flex justify-between items-center p-2 px-4 w-full bg-slate-100'>
+                    <button onClick={() => setIsModalOpen(true)} className='flex gap-2'>
+                        <i className='ri-add-fill mr-1'></i>
+                        <p>Add collaborator</p>
+                    </button>
+                    <button onClick={() => setIsSidePanelOpen(true)} className='p-2'>
+                        <i className="ri-group-fill"></i>
+                    </button>
+                </header>
+
+                <div className="conversation-area flex-grow flex flex-col">
+                    <div className="message-box p-1 flex-grow flex flex-col gap-1">
+                        <div className="message max-w-56 flex flex-col p-2 bg-slate-50 w-fit rounded-md">
+                            <small className='opacity-65 text-xs'>example@gmail.com</small>
+                            <p className='text-sm'>Lorem ipsum dolor sit amet.</p>
+                        </div>
+                        <div className="ml-auto message max-w-56 flex flex-col p-2 bg-slate-50 w-fit rounded-md">
+                            <small className='opacity-65 text-xs'>example@gmail.com</small>
+                            <p className='text-sm'>Lorem ipsum dolor sit amet.</p>
+                        </div>
+                    </div>
+                    <div className="inputField w-full flex">
+                        <input className='p-2 px-4 w-[85%] border-none outline-none' type="text" placeholder='Enter message'/>
+                        <button className='flex-grow px-3 bg-slate-950 text-white'>
+                            <i className="ri-send-plane-fill"></i>
+                        </button>
+                    </div>
+                </div>
+                
+                <div className={`sidePanel w-full h-full flex flex-col gap-2 bg-slate-50 absolute transition-all ${ isSidePanelOpen? 'translate-x-0': '-translate-x-full'} top-0`}>
+                    <header className='flex justify-between items-center px-4 p-2 bg-slate-200'>
+                        <h1 className='font-semibold text-lg'>Collaborators</h1>
+                        <button onClick={() => setIsSidePanelOpen(false)} className='p-2'>
+                            <i className="ri-close-line"></i>
+                        </button>
+                    </header>
+                    <div className="users flex flex-col gap-2">
+                        {project.users && project.users.map(user => {
+                            return(
+                                <div className="user cursor-pointer hover:bg-slate-200 p-2 flex gap-2 items-center">
+                                    <div className='aspect-square rounded-full w-fit h-fit flex items-center justify-center p-5 text-white bg-slate-600'>
+                                        <i className='ri-user-fill absolute'></i>
+                                    </div>
+                                    <h1 className='text-base'>{user.email}</h1>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
+            </section>
+            {isModalOpen && (
+                <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+                    <div className="bg-white p-4 rounded-md w-96 max-w-full relative">
+                        <header className='flex justify-between items-center mb-4'>
+                            <h2 className='text-xl font-semibold'>Select User</h2>
+                            <button onClick={() => setIsModalOpen(false)} className='p-2'>
+                                <i className="ri-close-fill"></i>
+                            </button>
+                        </header>
+                        <div className="users-list flex flex-col gap-2 mb-16 max-h-96 overflow-auto">
+                            {users.map(user => (
+                                <div key={user.id} className={`user cursor-pointer hover:bg-slate-200 ${Array.from(selectedUserId).indexOf(user._id)!=-1?'bg-slate-200': ''} p-2 flex gap-2 items-center`} onClick={() => handleUserClick(user._id)}>
+                                    <div className='aspect-square relative rounded-full w-fit h-fit flex items-center justify-center p-5 text-white bg-slate-600'>
+                                        <i className="ri-user-fill absolute"></i>
+                                    </div>
+                                    <h1 className='font-semibold text-lg'>{user.email}</h1>
+                                </div>
+                            ))}
+                        </div>
+                        <button
+                            onClick={addCollaborators}
+                            className='absolute bottom-4 left-1/2 transform -translate-x-1/2 px-4 py-2 bg-blue-600 text-white rounded-md'>
+                            Add Collaborators
+                        </button>
+                    </div>
+                </div>
+            )}
+        </main>
     )
 }
 
